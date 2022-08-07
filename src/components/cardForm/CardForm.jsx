8b12648db2699cb2412da72b9f55379388fc2125
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-07 00:42:06 -0400

    Successfully designed frontcard

--- src/components/cardForm/CardForm.jsx
+++ src/components/cardForm/CardForm.jsx
@@ -7,6 +7,33 @@ const CardForm = () => {
 
   return (
     <div className="cardForm">
+      <div className="frontCard">
+        <div className="frontImg">
+          <img
+            src="https://user-images.githubusercontent.com/70451928/183109745-b02ac54a-f22b-4399-b696-4f7701dfd0e5.png"
+            alt=""
+          />
+        </div>
+        <div className="frontCardInfo">
+          <p className="cardNumber">1234 1234 1234 1234</p>
+          <div className="cardDesc">
+            <p className="cardName">Amrindra</p>
+            <p className="cardDate">09/00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="backCard">
+        <img
+          src="https://user-images.githubusercontent.com/70451928/183109655-42c570a8-16d3-47b6-b366-c9e70f101491.png"
+          alt=""
+        />
+
+        <div className="backCardInfo">
+          <p>000</p>
+        </div>
+      </div>
+
       <div className="cardFormLeft">
         <img
           src="https://user-images.githubusercontent.com/70451928/183109770-900aace1-1b25-49b9-ae87-a071a5a21863.png"
@@ -51,6 +78,7 @@ const CardForm = () => {
               />
             </div>
           </div>
+
           <button>Confirm</button>
         </form>
       </div>
