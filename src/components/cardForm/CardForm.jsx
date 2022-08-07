bd7eadc125b5fd314538200c7bf666e5187bcd71
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-06 22:53:54 -0400

    Switched from CSS to SCSS and created handleSubmit function

--- src/components/cardForm/CardForm.jsx
+++ src/components/cardForm/CardForm.jsx
@@ -1,9 +1,41 @@
-import "./CardForm.css";
+import "./CardForm.scss";
 
 const CardForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="cardForm">
-      <h3>hello</h3>
+      <div className="cardFormLeft">
+        <img
+          src="https://user-images.githubusercontent.com/70451928/183109770-900aace1-1b25-49b9-ae87-a071a5a21863.png"
+          alt=""
+        />
+      </div>
+
+      <div className="cardFormRight">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">CARDHOLDER NAME</label>
+          <input type="text " placeholder="e.g. Jane Appleseed" />
+          <label htmlFor="">CARD NUMBER </label>
+          <input type="number " placeholder="e.g. 1234 5678 9123 0000" />
+
+          <div className="cardFormDate">
+            <div className="top">
+              <label htmlFor="">EXP. DATE(MM/YY)</label>
+              <label htmlFor="">CVC</label>
+            </div>
+
+            <div className="bottom">
+              <input type="number" placeholder="e.g. 123" />
+              <input type="number" placeholder="MM" />
+              <input type="number" placeholder="YY" />
+            </div>
+          </div>
+          <button>Confirm</button>
+        </form>
+      </div>
     </div>
   );
 };
