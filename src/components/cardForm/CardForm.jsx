9f80eea4461fe43102e0bd6da56a5b8c70912468
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-10 16:54:39 -0400

    Started submit result section

--- src/components/cardForm/CardForm.jsx
+++ src/components/cardForm/CardForm.jsx
@@ -7,20 +7,19 @@ const CardForm = () => {
   const [month, setMonth] = useState("00");
   const [year, setYear] = useState("00");
   const [cvc, setCVC] = useState("000");
+  const [focused, setFocused] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
-  // const handleChange = (e) => {
-  //   setCardName(e.target.value);
-  // };
-
   function format(splitDigit) {
     return splitDigit.toString().replace(/\d{4}(?=.)/g, "$& ");
   }
 
-  console.log(cardName);
+  const handleFocus = () => {
+    setFocused(true);
+  };
 
   return (
     <div className="cardForm">
@@ -65,72 +64,92 @@ const CardForm = () => {
       </div>
 
       <div className="cardFormRight">
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <label htmlFor="">CARDHOLDER NAME</label>
           <input
-            required
+            // required="true"
             className="cardHolderName"
+            name="cardName"
             type="text"
             placeholder="e.g. Jane Appleseed"
+            // pattern="[A-Za-z0-9]{1,20}"
+            focused={focused.toString()}
             onChange={(e) => setCardName(e.target.value)}
           />
           <span>Name can't be blank</span>
 
           <label htmlFor="">CARD NUMBER </label>
           <input
-            required
+            // required
             type="text"
+            name="cardNumber"
             placeholder="e.g. 1234 5678 9123 0000"
             pattern="[0-9]+"
             maxLength={16}
             onChange={(e) => setCardNumber(e.target.value)}
+            onBlur={handleFocus}
+            onFocus={() => setFocused(true)}
           />
           <span>Wrong format, numbers only</span>
 
           <div className="cardFormDate">
             <div className="top">
-              <label htmlFor="">EXP. DATE (MM/YY)</label>
-              <label className="cvcLabel" htmlFor="">
+              <label className="label">EXP. DATE (MM/YY)</label>
+              <label className="cvcLabel label" htmlFor="cvc">
                 CVC
               </label>
             </div>
 
             <div className="bottom">
               <input
-                required
+                // required
                 className="month"
+                name="month"
                 type="number"
                 placeholder="MM"
                 pattern="[0-9]+"
                 min={0}
                 maxLength={2}
                 onChange={(e) => setMonth(e.target.value)}
+                onBlur={handleFocus}
+                onFocus={() => setFocused(true)}
               />
               <input
-                required
+                // required
                 className="year"
                 type="number"
+                name="year"
                 placeholder="YY"
                 pattern="[0-9]+"
                 maxLength={2}
                 onChange={(e) => setYear(e.target.value)}
+                onBlur={handleFocus}
+                onFocus={() => setFocused(true)}
               />
               <input
-                required
+                // required
                 className="cvc"
                 type="text"
+                name="cvc"
                 placeholder="e.g. 123"
-                // value={cvc}
                 maxLength={3}
                 min={0}
                 onChange={(e) => setCVC(e.target.value)}
+                onBlur={handleFocus}
+                onFocus={() => setFocused(true)}
               />
             </div>
             <span>Can't be blank</span>
           </div>
 
           <button>Confirm</button>
-        </form>
+        </form> */}
+        <div className="result">
+          <img src="" alt="" />
+          <h3>Thank You!</h3>
+          <span>We've added your card details</span>
+          <button className="continue">Continue</button>
+        </div>
       </div>
     </div>
   );
